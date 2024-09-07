d9f127dbcc754959ea6760162d55bd04e583dfb2
Seleste  Sinete de Araujo <152917644+SelesteAra@users.noreply.github.com>
2024-09-07 15:22:59 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,25 @@
+function pesquisar(){
+    for (let dado of dados){
+        resultados += `
+       <div class="item-resultado">
+           <h2>
+               <a href="#" target="_blank"> ${dado.titulo}</a>
+           </h2>
+           <p class="descricao-meta">${dado.descricao}</p>
+           <a href= ${dado.link}  target="_blank">Mais informações</a>
+       </div>
+   `
+   section.innerHTML = resultados
+
+}
+
+// console.log(dados);
+
+let section = document.getElementById("resultados-pesquisa")
+
+console.log(section);
+
+let resultados = ""
+
+// para cada dado dentro da lista de dados
+
---
+++ dados.js
@@ -0,0 +1,21 @@
+
+let dados = [
+    {
+        titulo: "Granola Caseira",
+        descricao: "A granola caseira é uma opção deliciosa e nutritiva para um café da manhã completo ou um lanche rápido e prático. Ao prepará-la em casa, você tem total controle sobre os ingredientes e pode criar combinações personalizadas de acordo com seu paladar e necessidades nutricionais.",
+        link: <a href="https://www.receiteria.com.br/receita/granola-caseira/" target="_blank"> Mais informações</a>
+    },
+    {
+            titulo: "Iogurte grego com Frutas Vermelhas",
+            descricao: "Uma combinação refrescante e nutritiva! O iogurte grego fornece proteínas, as frutas vermelhas são ricas em antioxidantes e a granola adiciona crocância.",
+            link: <a href="https://www.tuasreceitas.com/iogurte-com-frutas" target="_blank">Mais informações</a>
+    },
+    {
+            titulo: "Smoothie Verde Detox",
+            descricao: " Um smoothie nutritivo e saboroso para começar o dia com energia. Rico em vitaminas e minerais, perfeito para uma dieta equilibrada.",
+            link: <a href="https://www.receitasfit.com.br/smoothie-verde" target="_blank">Mais informações</a>
+    }
+ ];
+
+
+   
